16ee52d92dc18b96bc6994e50dc754ea3cda138b
IGNACIO OLAGORTA VERA <ignacio.olagorta@utmac0018.local>
2018-04-03 04:10:07 -0700

    Initial Commit

---
+++ PrimerProyecto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimerProyecto
+//
+//  Created by IGNACIO OLAGORTA VERA on 3/4/18.
+//  Copyright © 2018 IGNACIO OLAGORTA VERA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
