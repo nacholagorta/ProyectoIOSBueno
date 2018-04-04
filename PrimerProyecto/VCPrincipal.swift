2c558347c326e1af44c190242d7db50eb3d3c17a
nacholagorta <nacholagorta@hotmail.com>
2018-04-04 08:24:20 -0700

    Avances de la tabla

---
+++ PrimerProyecto/VCPrincipal.swift
@@ -0,0 +1,37 @@
+//
+//  VCPrincipal.swift
+//  PrimerProyecto
+//
+//  Created by IGNACIO OLAGORTA VERA on 4/4/18.
+//  Copyright © 2018 IGNACIO OLAGORTA VERA. All rights reserved.
+//
+
+import UIKit
+
+class VCPrincipal: UIViewController {
+    
+    @IBOutlet var tbtablaCampeones:UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
